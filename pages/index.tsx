9a90615104872514f7700cc8ff551760af6da7e2
aeneasr <3372410+aeneasr@users.noreply.github.com>
2022-04-27 14:48:24 +0200

    fix: use @ory/client instead of @ory/kratos-client

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import styles from '../styles/Home.module.css'
 import { edgeConfig } from '@ory/integrations/next'
-import { Configuration, Session, V0alpha2Api } from '@ory/kratos-client'
+import { Configuration, Session, V0alpha2Api } from '@ory/client'
 import { AxiosError } from 'axios'
 import type { NextPage } from 'next'
 import Head from 'next/head'
